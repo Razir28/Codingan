d7d6173b016d7a7215ed9cf690efb546f6e33d12
Razir <112397370+Razir28@users.noreply.github.com>
2023-08-26 14:13:15 +0700

    Hover untuk detail: hapus = Codingan/HTML, CSS, JS/Belajar JS/index.html dan Codingan/HTML, CSS, JS/Belajar JS/main.js rename = Codingan/HTML, CSS, JS/Log In/index.html -> Codingan/HTML, CSS, JS/Log In (Tidak Murni)/index.html Codingan/HTML, CSS, JS/Log In/main.js -> Codingan/HTML, CSS, JS/Log In (Tidak Murni)/main.js Codingan/HTML, CSS, JS/Log In/style.css -> Codingan/HTML, CSS, JS/Log In (Tidak Murni)/style.css

--- Codingan/HTML, CSS, JS/Belajar JS/main.js
+++
@@ -1,7 +0,0 @@
-function submitData() {
-  let usrnm = document.getElementById("usrnm").value;
-  let psswrd = document.getElementById("psswrd").value;
-
-  console.log(usrnm);
-  console.log(psswrd);
-}
