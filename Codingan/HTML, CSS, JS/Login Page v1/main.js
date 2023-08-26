47e817ae447158ee00b27d77366894201418458b
Razir <112397370+Razir28@users.noreply.github.com>
2023-08-26 16:32:01 +0700

    modified

---
+++ Codingan/HTML, CSS, JS/Login Page v1/main.js
@@ -0,0 +1,41 @@
+// Kumpulan data user
+const userData = {
+  admin: {
+    Username: "admin",
+    Password: "admin123",
+  },
+
+  global: {
+    Username: "global",
+    Password: "global123",
+  },
+};
+
+function loginBtn() {
+  // Mendapatkan value (nilai) username & password yang user telah input
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
+
+  console.log("Username:", username);
+  console.log("Password:", password);
+
+  let cariUser = null; // Untuk mencari username dan password yang sama dengan yang user masukkan pada tag <input> di dalam userData
+
+  // Menggunakan Forin loop untuk mencarinya
+  for (const key in userData) {
+    if (
+      userData[key].Username == username &&
+      userData[key].Password == password
+    ) {
+      cariUser = userData[key];
+      break;
+    }
+  }
+
+  // Bila sudah ketemu
+  if (cariUser !== null) {
+    console.log(cariUser);
+  } else {
+    console.log("Username dan atau Password salah.");
+  }
+}
