83a1095a5aeff1ca1e0e987a852254e736cfca13
Razir <112397370+Razir28@users.noreply.github.com>
2023-08-26 20:26:33 +0700

    modified

--- Codingan/HTML, CSS, JS/Login Page A/main.js
+++ Codingan/HTML, CSS, JS/Login Page A/main.js
@@ -16,10 +16,13 @@ const userData = {
     Username: "razir28",
     Password: "razir280606",
     Access: "Limited",
-  }
+  },
 };
 
 function loginBtn() {
+  // Untuk div yang fungsinya menampilkan berhasil atau gagalnya login
+  const announceContainer = document.getElementById("announceContainer");
+
   // Mendapatkan value (nilai) username & password yang user telah input
   const username = document.getElementById("username").value;
   const password = document.getElementById("password").value;
@@ -43,7 +46,28 @@ function loginBtn() {
   // Bila sudah ketemu
   if (cariUser !== null) {
     console.log(cariUser);
+    announceContainer.textContent = "Login berhasil";
+    announceContainer.style.backgroundColor = "green";
   } else {
     console.log("Username dan atau Password salah.");
+    announceContainer.textContent = "Username dan atau Password salah";
+    announceContainer.style.backgroundColor = "#ff0000";
   }
+
+  announceContainer.style.display = "block"; // State awal
+
+  setTimeout(() => {
+    announceContainer.style.opacity = "1"; // Fade in
+
+    // Set timeout untuk fade out dan menyembunyikan pesan setelah 3 detik (3000 ms)
+    setTimeout(() => {
+      announceContainer.style.opacity = "0"; // Fade out
+      setTimeout(() => {
+        announceContainer.style.display = "none";
+      }, 500); // Menunggu animasi fade out selesai sebelum menyembunyikan pesa
+    }, 3000); // Menyembunyikan pesan setelah 3 detik (3000 ms)
+  }, 100); // Delay untuk menampilkan pesan selama 100 ms
+
+  // Mencegah agar halaman tidak refresh
+  return false;
 }
